21a9771a046e33902899d25ec192bb7960d56772
David <54193908+dh337071@users.noreply.github.com>
2024-12-14 15:10:59 -0600

    Add files via upload
    add custom script.

---
+++ customMapFormat.js
@@ -0,0 +1,39 @@
+var customMapFormat = {
+    name: "Thetawave wave file",
+    extension: "ron",
+
+    write: function(map, fileName) {
+        var m = {
+            width: map.width,
+            height: map.height,
+            objects: ""
+        };
+
+        for (var i = 0; i < map.layerCount; ++i) {
+            var layer = map.layerAt(i);
+            if (layer.isObjectLayer) {
+                var mapString = "";
+                mapString = mapString.concat("(\n\tformation_spawnables: [ \n")
+                for (y = 0; y < layer.objectCount; ++y) {
+                    mapObject = layer.objectAt(y);
+                    mapString = mapString.concat("\t\t( \n ")
+                    mapString = mapString.concat("\t\t\tspawnable_type: ")
+                    mapString = mapString.concat(mapObject.resolvedProperty("enemy_type"));
+                    mapString = mapString.concat(",\n\t\t\tposition: (");
+                    mapString = mapString.concat(mapObject.x);
+                    mapString = mapString.concat(", ");
+                    mapString = mapString.concat(mapObject.y);
+                    mapString = mapString.concat("), \n\t\t),\n");
+                }
+                mapString = mapString.concat("\n\t],\n");
+                m.objects = m.objects.concat(mapString);
+            }
+        }
+
+        var file = new TextFile(fileName, TextFile.WriteOnly);
+        file.write(m.objects);
+        file.commit();
+    },
+}
+
+tiled.registerMapFormat("ron", customMapFormat)
